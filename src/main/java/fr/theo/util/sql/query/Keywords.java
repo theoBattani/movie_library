ee7afa01015a2ad261dae5910695607335d97890
theoBattani <84023706+theoBattani@users.noreply.github.com>
2021-10-21 15:38:24 +0200

    Update Keywords.java

--- src/main/java/fr/theo/util/sql/query/Keywords.java
+++ src/main/java/fr/theo/util/sql/query/Keywords.java
@@ -49,10 +49,10 @@ public class Keywords {
   public static final String HAVING                 = "HAVING";
   public static final String IN                     = "IN";
   public static final String INDEX                  = "INDEX";
-  public static final String INNER_JOIN             = "INNER_JOIN";
+  public static final String INNER_JOIN             = "INNER JOIN";
   public static final String INSERT_INTO            = "INSERT INTO";
   public static final String INSERT_INTO_SELECT     = "INSERT INTO SELECT";
-  public static final String IS_NULL                = "IS_NULL";
+  public static final String IS_NULL                = "IS NULL";
   public static final String IS_NOT_NULL            = "IS NOT NULL";
   public static final String JOIN                   = "JOIN";
   public static final String LEFT_JOIN              = "LEFT JOIN";
